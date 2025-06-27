7236fd03da708e3e8889f8ffbbbb504dab53ffb5
John Voltz <john@spy-free.net>
2025-06-27 14:03:48 -0400

    Save user settings to config.ini

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
+    // Keep running even when dialogs are closed since the tray icon
+    // is the primary interface
+    app.setQuitOnLastWindowClosed(false);
 
     TrayController controller;
     controller.show();
--- src/traycontroller.cpp
+++ src/traycontroller.cpp
@@ -1,21 +1,30 @@
 #include "traycontroller.h"
+#include "settingsdialog.h"
+#include <QSettings>
 #include <QApplication>
 #include <QIcon>
 #include <QNetworkRequest>
 #include <QUrl>
 #include <QAuthenticator>
 #include <QTimer>
+#include <QFile>
+#include <QDir>
+#include <QCoreApplication>
 
 TrayController::TrayController()
     : actionOn("Turn On", this),
       actionOff("Turn Off", this),
+      actionSettings("Settings", this),
       actionQuit("Quit", this)
 {
     trayIcon.setIcon(QIcon::fromTheme("network-wired"));
     trayIcon.setToolTip("ezOutlet5 Controller");
 
+    loadSettings();
+
     menu.addAction(&actionOn);
     menu.addAction(&actionOff);
+    menu.addAction(&actionSettings);
     menu.addSeparator();
     menu.addAction(&actionQuit);
 
@@ -23,6 +32,7 @@ TrayController::TrayController()
 
     connect(&actionOn, &QAction::triggered, this, &TrayController::turnOn);
     connect(&actionOff, &QAction::triggered, this, &TrayController::turnOff);
+    connect(&actionSettings, &QAction::triggered, this, &TrayController::openSettings);
     connect(&actionQuit, &QAction::triggered, this, &TrayController::quit);
 }
 
@@ -44,10 +54,12 @@ void TrayController::quit() {
 }
 
 void TrayController::sendCommand(bool on) {
-    QString urlStr = QString("http://%1/cgi-bin/control.cgi?user=%2&passwd=%1&target=1&control=%3")
-        .arg(outletIp)
-        .arg(username)
-        .arg(on ? "1" : "0");
+    QString urlStr =
+        QString("http://%1/cgi-bin/control.cgi?user=%2&passwd=%3&target=1&control=%4")
+            .arg(outletIp)
+            .arg(username)
+            .arg(password)
+            .arg(on ? "1" : "0");
 
     QUrl url(urlStr);
     QNetworkRequest request(url);
@@ -58,4 +70,36 @@ void TrayController::sendCommand(bool on) {
     request.setRawHeader("Authorization", "Basic " + base64);
 
     networkManager.get(request);
+}
+
+void TrayController::openSettings() {
+    SettingsDialog dialog;
+    dialog.setIp(outletIp);
+    dialog.setUsername(username);
+    dialog.setPassword(password);
+
+    if (dialog.exec() == QDialog::Accepted) {
+        outletIp = dialog.ip();
+        username = dialog.username();
+        password = dialog.password();
+        saveSettings();
+    }
+}
+
+void TrayController::loadSettings() {
+    QSettings settings(QDir(QCoreApplication::applicationDirPath()).filePath("config.ini"),
+                       QSettings::IniFormat);
+    if (QFile::exists(settings.fileName())) {
+        outletIp = settings.value("outletIp", outletIp).toString();
+        username = settings.value("username", username).toString();
+        password = settings.value("password", password).toString();
+    }
+}
+
+void TrayController::saveSettings() {
+    QSettings settings(QDir(QCoreApplication::applicationDirPath()).filePath("config.ini"),
+                       QSettings::IniFormat);
+    settings.setValue("outletIp", outletIp);
+    settings.setValue("username", username);
+    settings.setValue("password", password);
 }
